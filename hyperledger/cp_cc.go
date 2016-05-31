b4a83034ceeb424429e250be4119d2a09d54657e
shambhavi1993 <kishoreshambhavi@gmail.com>
2016-05-31 15:35:37 +0530

    Update cp_cc.go

--- hyperledger/cp_cc.go
+++ hyperledger/cp_cc.go
@@ -88,6 +88,8 @@
 		House	  string  `json:"house"`
 		Street	  string  `json:"street"`
 		Pin	  string  `json:"pin"`
+		Email	  string  `json:"email"`
+		Mobile	  string  `json:"mobile"`
 		Owner     string  `json:"owner"`
 		Issuer    string  `json:"issuer"`
 		IssueDate string  `json:"issueDate"`
