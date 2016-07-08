9822f2a7f1b0cbaf173ac27034646e5930e8edcb
Ankit <ankitkamra54@gmail.com>
2016-07-08 10:13:25 +0530

    Update cp_cc.go

--- hyperledger/cp_cc.go
+++ hyperledger/cp_cc.go
@@ -93,6 +93,7 @@
 		Mobile	  string  `json:"mobile"`
 		Fmrdata	  string  `json:"fmrdata"`
 		Owner     string  `json:"owner"`
+		Filename  string  `json:"filename"`
 		Issuer    string  `json:"issuer"`
 		IssueDate string  `json:"issueDate"`
 	}
